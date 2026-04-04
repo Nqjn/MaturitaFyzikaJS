7b768330a2c01c6cbced4b5f88c6f6795646f504
Nqjn <jan.navratil04@gmail.com>
2026-04-04 19:26:33 +0200

    Konečně funkční commit

---
+++ app.js
@@ -0,0 +1,73 @@
+// app.js
+let aktualniOtazka = null;
+
+function generujPolicka() {
+    const cislo = document.getElementById('questionPicker').value;
+    const kontejner = document.getElementById('dynamicInputs');
+    const nadpis = document.getElementById('displayTopic');
+    
+    // Vyčištění
+    kontejner.innerHTML = "";
+    document.getElementById('resultMsg').innerText = "";
+
+    // MATURITA_DATA pochází ze souboru data.js
+    if (MATURITA_DATA[cislo]) {
+        aktualniOtazka = MATURITA_DATA[cislo];
+        nadpis.innerText = `Téma: ${aktualniOtazka.question}`;
+
+        // Pro každý podbod vytvoříme input
+        aktualniOtazka.section.forEach((bod, i) => {
+            const skupina = document.createElement('div');
+            skupina.className = "input-group";
+            skupina.innerHTML = `
+                <label>Bod ${i + 1}:</label>
+                <input type="text" class="user-answer" placeholder="Doplňte název podbodu...">
+                <button class="check-btn" onclick="zkontrolujOdpoved(${i}, this)">Zkontrolovat</button>
+            `;
+            kontejner.appendChild(skupina);
+        });
+
+
+        document.getElementById('submitBtn').style.display = "block";
+    } else {
+        alert("Tahle otázka v databázi není!");
+    }
+}
+
+function zkontrolujOdpovedi() {
+    const vstupy = document.querySelectorAll('.user-answer');
+    let skore = 0;
+
+    vstupy.forEach((input, index) => {
+        const odpoved = input.value.trim().toLowerCase();
+        const spravne = aktualniOtazka.section[index].trim().toLowerCase();
+
+        if (odpoved === spravne) {
+            input.className = "user-answer correct";
+            skore++;
+        } else {
+            input.className = "user-answer wrong";
+            input.value += ` (Správně: ${aktualniOtazka.section[index]})`;
+        }
+    });
+
+    document.getElementById('resultMsg').innerText = `Hotovo! Uhodl jsi ${skore} z ${vstupy.length}.`;
+}
+
+function zkontrolujOdpoved(index, tlacitko) {
+
+    const skupina = tlacitko.parentElement;
+    const input = skupina.querySelector('.user-answer');
+    const odpoved = input.value.trim().toLowerCase();
+
+    const spravnaOdpoved = aktualniOtazka.section[index].trim().toLowerCase();
+
+    if (odpoved === spravnaOdpoved) {
+        input.className = "user-answer correct";
+    } else {
+        input.className = "user-answer wrong";
+        input.value += ` (Správně: ${aktualniOtazka.section[index]})`;
+    }
+
+
+}
---
+++ data.js
@@ -0,0 +1,288 @@
+const MATURITA_DATA = {
+    "1": {
+        "question": "Kinematika",
+        "section": [
+            "Hmotný bod",
+            "Poloha hmotného bodu",
+            "Trajektorie vs Dráha",
+            "Rychlost",
+            "Klasifikace pohybů",
+            "Rovnoměrně přímočarý pohyb",
+            "Zrychlení",
+            "Pohyby se zrychlením",
+            "Skládání rychlostí",
+            "Pohyb po kružnici"
+        ]
+    },
+    "2": {
+        "question": "Dynamika",
+        "section": [
+            "Síla",
+            "Účinky síly",
+            "Skládání a rozklad sil",
+            "1. NWZ",
+            "2. NWZ",
+            "3. NWZ",
+            "Tření",
+            "Dostředivá síla",
+            "Inerciální a neinerciálníá soustava",
+            "Mechanická práce",
+            "Mechanická energie",
+            "Výkonn a Účinnost"
+        ]
+    },
+    "3": {
+        "question": "ZÁKONY ZACHOVÁNÍ ENERGIE V RŮZNÝCH OBLASTECH FYZIKY",
+        "section": [
+            "Práce vs Energie",
+            "Zákon zachování mechanické energie",
+            "Vnitřní energie",
+            "Bernoulliho rovnice",
+            "1. termodynamický zákon",
+            "Kalorimetrická rovnice",
+            "Energie mechanického oscilátoru",
+            "Energie elektromagnetického oscilátoru",
+            "Fotoefekt",
+            "Comptonův jev"
+        ]
+    },
+    "4": {
+        "question": "Mechanika tuhého tělesa",
+        "section": [
+            "Tůhé těleso",
+            "Pohyb tuhého tělesa",
+            "Moment síly",
+            "Momentová věta",
+            "Skládání sil",
+            "Dvojice sil",
+            "Těžiště",
+            "Rovnovážná poloha",
+            "Kinetikcá energie"
+        ]
+    },
+    "5": {
+        "question": "Mechanika kapalin a plynů",
+        "section": [
+            "Vlastnosti Tekutin",
+            "Tlak",
+            "Pascalův zákon",
+            "Hydrostatická tlak",
+            "Tlak vzduchu",
+            "Archimédův zákon",
+            "Proudění",
+            "Rovnice kontinuity",
+            "Bernoulliho rovnice",
+            "Proudění reálné kapaliny",
+            "Odporová síla"
+        ]
+    },
+    "6": {
+        "question": "Gravitační pole a Keplerovy zákony",
+        "section": [
+            "NW gravitační zákon",
+            "Typy polí",
+            "Gravitační a tíhové zrychlení",
+            "Tíha",
+            "Vrhy",
+            "Keplerovy zákony"
+        ]
+    },
+    "7": {
+        "question": "Elektrické pole",
+        "section": [
+            "Elektrický náboj",
+            "Coulombůlv zákon",
+            "Intezita elektrického pole",
+            "Typy polí",
+            "Práce",
+            "Potenciál a Napětí",
+            "Náboj na vodivých tělesech",
+            "Elektrostatická indukce",
+            "Atomová polarizace",
+            "Kapacita vodiče",
+            "Kondenzátor"
+        ]
+    },
+    "8": {
+        "question": "Struktura a vlastnosti plynů",
+        "section": [
+            "Ideální plyn",
+            "Střední kvadratická rychlost",
+            "Rovnice tlaku",
+            "Stavová změna",
+            "Stavová rovnice",
+            "IZO děje",
+            "Kruhové děje",
+            "2. Termodynamický zákon",
+            "Tepelelné motory"
+        ]
+    },
+    "9": {
+        "question": "Struktura a vlastnosti pevných látek a kapalin",
+        "section": [
+            "Dělení pevných látek",
+            "Krystalická mřížka",
+            "Deformace",
+            "Hookův zákon",
+            "Teplotní roztažnost",
+            "Povrchová vrstva kapalin",
+            "Povrchová energie",
+            "Povrchové napětí",
+            "Kapilarita",
+            "Objemová roztažnost",
+            "Anomálie vody"
+        ]
+    },
+    "10": {
+        "question": "Skupenské přeměny látek",
+        "section": [
+            "Typy skupenství",
+            "Fázový diagram",
+            "Tání a Tuhnutí",
+            "Vypařování a Kondenzace",
+            "Vypařování a var",
+            "Sublimace a Desublimace",
+            "Sytá pára",
+            "Vlhkost ovzduší"
+        ]
+    },
+    "11": {
+        "question": "Elektrický proud v látkách, zákony stejnosměrného elektrického proudu",
+        "section": [
+            "Proud",
+            "Zdroj",
+            "Voltampérová charakteristika",
+            "Ohmův zákon pro část obvodu",
+            "Odpor",
+            "Spojování rezistorů",
+            "Ohmův zákon pro uzavřený obvod",
+            "Zátéžová charakteristika",
+            "Ochrana proti zkratu",
+            "Práce",
+            "Kirchhoffy zákony"
+        ]
+    },
+    "12": {
+        "question": "Vedení elektrického proudu v polovodičích",
+        "section": [
+            "Typy polovodičů",
+            "Vlastní vodivost",
+            "Příměsové polovodiče",
+            "PN přechod",
+            "Dioda",
+            "Tranzistor",
+            "Převodní charakteristika",
+            "Integrované obvody"
+        ]
+    },
+    "13": {
+        "question": "Vedení elektrického proudu v kapalinách, plynech a ve vakuu",
+        "section": [
+            "Elktrolýza",
+            "Faradayovy zákony",
+            "VA charakteristika v kapalinách",
+            "Galvanický článek",
+            "Ionizace plynu",
+            "VA charakteristika v plynech",
+            "Výboje za atmosférického tlaku",
+            "Výboje za sniženého tlaku",
+            "Katodové a kanálové záření"
+        ]
+    },
+    "14": {
+        "question": "Stacionární magnetické pole",
+        "section": [
+            "Magnetické pole",
+            "Vodič s proudem",
+            "Magnetická síla a indukce",
+            "Rovnoběžné vodiče s proudem",
+            "Magnetické pole cívky",
+            "Nabitá částice v magnetickém poli",
+            "Lorentzova síla",
+            "Magnetické vlastnosti látek",
+            "Hysterezní smyčka",
+            "Hallův jev"
+        ]
+    },
+    "15": {
+        "question": "Nestacionární magnetické pole",
+        "section": [
+            "Elektromagnetická indukce",
+            "Magnetický indukční tok",
+            "Závit v magnetickém poli",
+            "Indykovaný proud",
+            "Vířivé proudy",
+            "Vlastní indukce",
+            "Přechodný děj",
+            "Energie magnetického pole cívky"
+        ]
+    },
+    "16": {
+        "question": "Kmitavý pohyb",
+        "section": [
+            "Kmitavý pohyb",
+            "Harmonické kmitání",
+            "Rychlost a zrychlení",
+            "Fáze kmitavého pohybu",
+            "Skládání kmitání",
+            "Dynamika kmitavého pohybu",
+            "Kyvadalo a pružinový oscilátor",
+            "vlastní a nucené kmitání",
+            "Rezonance"
+        ]
+    },
+    "17": {
+        "question": "Střídavý proud",
+        "section": [
+            "Střídavý proud",
+            "Obvod s odporem",
+            "Obvod s indukčností",
+            "Obvod s kapacitou",
+            "RLC obvod v sérii",
+            "Výkon"
+        ]
+    },
+    "18": {
+        "question": "Střídavý proud v energetice",
+        "section": [
+            "Princip vzniku",
+            "Alternátor",
+            "Trojfázová soustava",
+            "Zapojení spotřebičů většího výkonu",
+            "Elektromotor",
+            "Transformátor",
+            "Přenost energie",
+            "Typy elektráren"
+        ]
+    },
+    "19": {
+        "question": "Vlnění",
+        "section": [
+            "Vznik a druhy",
+            "Rovnice postupné vlny",
+            "Interference",
+            "Stojaté vlnění",
+            "Chvění mechanických soustav",
+            "Huygensův princip",
+            "Odraz",
+            "Lom",
+            "Ohyb",
+            "Zvuk",
+            "Vlastnosti zvuku",
+            "Ultrazvuk",
+            "Infrazvuk"
+        ]
+    },
+    "20": {
+        "question": "Optické zobrazení",
+        "section": [
+            "Světlo",
+            "Odraz a lom",
+            "disperze",
+            "Zobrazení zrcadly",
+            "Zobrazení čočkami",
+            "Subjektování zobrazovací přístroje",
+            "Objektivní zobrazovací přístroje"
+        ]
+    }
+}
